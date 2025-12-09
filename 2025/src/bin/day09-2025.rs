2f251c759b9180e48ea4442e982f9e45ec408254
Felix Baum <baum-felix@gmx.de>
2025-12-09 22:59:26 +0100

    feat: day 8 part two

--- 2025/src/bin/day09-2025.rs
+++ 2025/src/bin/day09-2025.rs
@@ -33,8 +33,77 @@ fn part_one(input: &str) -> Option<usize> {
     Some(max)
 }
 
-fn part_two(_input: &str) -> Option<u64> {
-    None
+#[derive(Debug)]
+struct Line {
+    p1: Point,
+    p2: Point,
+}
+
+impl Line {
+    fn new(p1: Point, p2: Point) -> Line {
+        Line { p1, p2 }
+    }
+}
+
+struct Rectangle {
+    min: Point,
+    max: Point,
+}
+
+impl Rectangle {
+    fn new(c1: Point, c2: Point) -> Rectangle {
+        let min = Point::new(c1.x().min(c2.x()), c1.y().min(c2.y()));
+        let max = Point::new(c1.x().max(c2.x()), c1.y().max(c2.y()));
+        Rectangle { min, max }
+    }
+
+    fn intersects(&self, line: &Line) -> bool {
+        let lxmin = line.p1.x().min(line.p2.x());
+        let lxmax = line.p1.x().max(line.p2.x());
+        let lymin = line.p1.y().min(line.p2.y());
+        let lymax = line.p1.y().max(line.p2.y());
+
+        // check if the line is strictly within the rectangle
+        // check different component based on line orientation
+        if lxmin == lxmax {
+            let lx = lxmin;
+            (self.min.x() < lx && lx < self.max.x())
+                && ((lymin <= self.min.y() && lymax > self.min.y())
+                    || (lymin < self.max.y() && lymax >= self.max.y()))
+        } else {
+            let ly = lymin;
+            (self.min.y() < ly && ly < self.max.y())
+                && ((lxmin <= self.min.x() && lxmax > self.min.x())
+                    || (lxmin < self.max.x() && lxmax >= self.max.x()))
+        }
+    }
+}
+
+fn part_two(input: &str) -> Option<usize> {
+    let points = parse(input);
+    let mut lines: Vec<_> = points
+        .windows(2)
+        .map(|ps| Line::new(ps[0], ps[1]))
+        .collect();
+    lines.push(Line::new(points[0], points[points.len() - 1]));
+
+    let mut max = 0;
+    for p in points.iter() {
+        for q in points.iter() {
+            let d = p.area(q);
+            if d > max {
+                let r = Rectangle::new(*p, *q);
+
+                if lines.iter().any(|l| r.intersects(l)) {
+                    continue;
+                }
+
+                max = d;
+            }
+        }
+    }
+
+    Some(max)
 }
 
 #[cfg(test)]
